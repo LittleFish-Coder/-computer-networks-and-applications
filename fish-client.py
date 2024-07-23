aea4de6ed17d9c189bfa4547f9e5f47f72eeff44
LittleFish-Coder <adrianyu890317@gmail.com>
2024-07-23 21:12:45 +0800

    finish 1~6

--- fish-client.py
+++ fish-client.py
@@ -2,46 +2,49 @@ import sys
 import socket
 import random
 
-def construct_query_message(qid, qtype, qname):
-    message = f"{qid} {qtype} {qname}"
+
+def construct_query_message(qid, qname, qtype):
+    message = f"{qid} {qname} {qtype}"
     return message.encode()
+
 
 def client(server_port, qname, qtype, timeout):
     # client message and info
     qid = random.randint(0, 65535)  # Assign a randomly generated, 16-bit unsigned integer as the qid
-    query_message = construct_query_message(qid, qtype, qname)
+    query_message = construct_query_message(qid, qname, qtype)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.settimeout(timeout)
-    
-    server_address = ('localhost', server_port)
+
+    server_address = ("localhost", server_port)
 
     # connect to server
     try:
-        print(f"Sending query: {query_message}")
+        # print(f"Sending query: {query_message}\n")
         client_socket.sendto(query_message, server_address)
 
         # wait for response
         response_message, _ = client_socket.recvfrom(2048)
-        response_parts = response_message.decode().split("\n")
-        
+        response_parts = response_message.decode()
+
         print(f"ID: {qid}\n")
         print("QUESTION SECTION: ")
-        print("TBD")
+        print(f"{qname} {qtype}\n")
         print("ANSWER SECTION: ")
-        print("TBD")
-        
+        print(f"{response_parts}\n")
+
     except socket.timeout:
         print("timed out")
     finally:
         client_socket.close()
 
+
 if __name__ == "__main__":
 
     if len(sys.argv) != 5:
         print("Usage: python3 client.py server_port qname qtype timeout")
-        raise 'Invalid arguments'
-    
-    ''' Example
+        raise "Invalid arguments"
+
+    """ Example
     $ python3 client.py 54321 example.com. A 5
     ID: 17564
 
@@ -55,7 +58,7 @@ if __name__ == "__main__":
     2024-05-21 19:20:31.750 rcv 62370: 17564 example.com. A (delay: 3s)
     2024-05-21 19:20:34.756 snd 62370: 17564 example.com. A
     
-    '''
+    """
 
     server_port = int(sys.argv[1])
     qname = sys.argv[2]
--- fish-server.py
+++ fish-server.py
@@ -1,5 +1,7 @@
 import sys
 import socket
+import random
+import time
 
 
 def load_master_file():
@@ -15,45 +17,66 @@ def load_master_file():
             records[domain].append((rtype, data))
     return records
 
-def handle_query(query_message, records):
-    """Handle the query message and return the response"""
-    qid, qtype, qname = query_message.decode().split()
-    print(f"Query: {qid} {qtype} {qname}")
 
-    response = ""
-    if qname in records:
+def handle_query(qid, qname, qtype, records):
+    """Handle the query message and return the response"""
+    # print(f"Query: {qid} {qname} {qtype}")
+
+    result = []
+    if qname in records.keys():
         for rtype, data in records[qname]:
-            response += f"{qname} {rtype} {data}\n"
+            if rtype == "CNAME" and qtype != "CNAME":  # G.5, G.6 example
+                result.append(f"{qname} {rtype} {data}")
+                new_qname = data
+                result += handle_query(qid, new_qname, qtype, records)
+            elif rtype == qtype:  # G.1, G.2, G.3, G.4 example
+                result.append(f"{qname} {rtype} {data}")
     else:
-        response = f"{qname} {qtype} - Error: HOST NOT FOUND\n"
-    
-    return response.encode()
-    pass
+        result.append(f"{qname} {qtype} -")
+
+    return result
+
 
 def start_server(server_port):
     """Start the server"""
     print("Starting server...")
     records = load_master_file()
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind(('localhost', server_port))
+    server_socket.bind(("localhost", server_port))
     print(f"Server listening on port {server_port}")
     print(f"Current IP: {socket.gethostbyname(socket.gethostname())}")
+    print(f"Server log: \n")
 
-    
     # keep listening for incoming requests
     while True:
         message, client_address = server_socket.recvfrom(2048)
-        print(f"Received message from {client_address}, Message: {message}")
-        handle_query(message, records)
-        server_socket.sendto(message, client_address)
+        qid, qname, qtype = message.decode().split()
+        # print(f"Received message from {client_address}, Message: {message}")
+
+        # start time
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S.%f", time.localtime())
+        delay = random.randint(0, 4)  # simulate delay
+        print(f"{timestamp} rcv {client_address[1]}: {qid} {qname} {qtype} (delay: {delay}s)")
+
+        # handle query
+        result = handle_query(qid, qname, qtype, records)
+        # simulate delay
+        time.sleep(delay)
+        server_response = "\n".join(result).encode()
+        server_socket.sendto(server_response, client_address)
+
+        # end time
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S.%f", time.localtime())
+        print(f"{timestamp} snd {client_address[1]}: {qid} {qname} {qtype} ")
 
     server_socket.close()
+
 
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
         print("Usage:  python3 server.py server_port")
-        raise 'Invalid arguments'
-    
+        raise "Invalid arguments"
+
     server_port = int(sys.argv[1])
     start_server(server_port)
